de29973cd88de1cd029d70c5c977bb4ef20c0523
Tyler Bonnell <tbonnell@uw.edu>
2018-02-20 18:44:37 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+[Theme](https://github.com/BlackrockDigital/startbootstrap-freelancer)
+[Dictionary](http://www.completewermosguide.com/ewokese.html)
---
+++ js/ewokese.js
@@ -0,0 +1,235 @@
+text_dict = [
+    "Aargutcha (ar gooch ah) v.  1. Pull.", 
+    "Acha (ah chah)  1. Okay.  2. All right.", 
+    "Ah-ah (ah ah) n.  1. Water.", 
+    "Akeeata (ah kee ah tah) v.  1. Hear.", 
+    "Allayloo (ah lay loo) v.  1. Celebrate.", 
+    "Ando (an doh) adj.  1. Forward.", 
+    "Arandee (ar ahn dee) v.  1. Listen.", 
+    "Azar (ay zar) n.  1. Magic.", 
+    "Bingee (bin jee) v.  1. Guide.", 
+    "Bi toto (bee toh toh) n.  1. Direction.", 
+    "Bok chuu-ock (bahk choo ahk) adj.  1. Far away. ", 
+    "Cha (chah)  1. Them.", 
+    "Chak (chahk)  1. Yes.", 
+    "Che (chee) adj.  1. Free as in liberty..", 
+    "Chesl (chess uhl) v.  1. Bring.  2. Fetch.", 
+    "Chiotto (chee oh toh)  1. May I have..?.", 
+    "Chiutatal (chy oo tah tail) n.  1. Moon.", 
+    "Choo doo (choo doo) adj.  1. Enough.", 
+    "Chuck (chuk) adj.  1. Much.", 
+    "Churi (chor ee) n.  1. Mountain.", 
+    "Chyasee (chy ah see) v.  1. Help.", 
+    "Coatee-cha (kote tee chah) v.  1. Celebrate.", 
+    "Coro (kor oh)  1. Can you...?.", 
+    "Cupee (cupp ee) n.  1. Cap as in ice cap on a mountain. ", 
+    "Danthee (dan thay)  1. Maybe.", 
+    "Danvay (dan vay)  1. Be careful.", 
+    "Dee fratta (dee frah tah)  1. That's.  2. That is.", 
+    "Deej (deej) n.  1. Father.", 
+    "Den (den)  1. No.", 
+    "De oppra (dee opp rah) adj.  1. Upper.  2. Top.", 
+    "Drik (drik) adj.  1. Happy.", 
+    "Drin (drin) adj.  1. Sick.", 
+    "Dutak (doo tok) n.  1. Arrow.", 
+    "Ee chee wa maa! (ee chee wah mah)  1. Wow.  2. Gee whiz!", 
+    "Ee choya! (ee choy ah) interj. 1. Hey.", 
+    "Eedada (ee dah dah) adj.  !. Over.", 
+    "Eekeekeek (eek eek eek) n. 1. Forest.", 
+    "Eekeetuhkuh (ee kee tuh kuh) n.  1. Axe.", 
+    "Eetee (ee tee) adj.  1. Long.", 
+    "Eleeo (ee lee oh) adj.  1. Never.", 
+    "Eleeoth (ee lee awth) adj.  1. Far.", 
+    "Ehda (ay dah) adj.  1. Evil.  2. Bad.", 
+    "Ehshtee (esh tee) v.  1. Give.", 
+    "Ekla (ek lah) adj.  1. Big.", 
+    "Enenah (en en ah)  1. Where.", 
+    "Entzahee roda (ents zah hee roh dah) v.  1. Cook.", 
+    "E s'eesht (ee seesht) v.  1. Kill.", 
+    "Etke (ett keh) adj.  1. More.", 
+    "Feef (feef) n.  1. Plant.", 
+    "Fektur (fek tur) n.  1. Medicine.", 
+    "Freet (freet) n.  1. Sister.", 
+    "Fruk (fruk) n.  1. Brother.", 
+    "Fulu (foo loo) n.  1. Circle.", 
+    "Geetch (geech) v.  1. Push.", 
+    "Gleeg (gleeg) n.  1. Drink.", 
+    "Glek (glek) adj.  1. Sad.", 
+    "Gooka (goo kah) n.  1. Shelter.", 
+    "Goopa (goo pah)  1. Hi.", 
+    "Goot (goot) adj.  1. Good.", 
+    "Graks (graks) n.  1. Monster.", 
+    "Grek (grek) n.  1. Hands.", 
+    "Grenchicit (gren chik it)  1. Hold on.", 
+    "Gyeesh (gyeesh)  1. Please.", 
+    "Heth (heth) n.  1. Pond.", 
+    "Hutar (hoo tar) n.  1. Danger.", 
+    "Hveetin (he veet in) n.  1. Glider.", 
+    "Jad (jad) adj.  1. Down.", 
+    "Jadgreh (jad greh) n.  1. Foot. ", 
+    "Jarat (jah raht) n.  1. Branch.", 
+    "Jeejee (jee jee) n.  1. Face.", 
+    "Jeerota (jee roe tah) n.  1. Friend.", 
+    "Kash (kahsh)  1. How.", 
+    "Kla (klah)  1. From.", 
+    "Kna Naa (kuh nah nah) n.  1. Spirit tree.", 
+    "Kra (krah) adj.  1. Ready.", 
+    "Kreeth (kreeth) n.  1. Cave.", 
+    "Kush (koosh)  1. Who.", 
+    "Luka yit (loo kah yitt) n.  1. Pixie.", 
+    "Lulalar (loo lah lahr) v.  1. Sing.", 
+    "Lungee (lunj ee) adj.  1. Lost.", 
+    "Lurd (lerd) adj.  1. Dumb.  2. Silly", 
+    "Luu (loo) adj.  1. Beautiful.", 
+    "Luufi (loo fee) n. 1. Flower.", 
+    "Manna manna (man ah man ah)  n.  1. Food.", 
+    "Meechoo (mee choo)  1. I.  2. Me.", 
+    "Mookiee (moo kee) n.  1. Female baby Ewok.  2. Wokling. (1)", 
+    "Na-chin (nah chin) n.  1. Tribe.", 
+    "Na goo (nah goo) v.  1. Stop.", 
+    "Neetuhl (nee tul) adj.  1. Early. ", 
+    "Ni gata (nee gah tah)  1. Where is..?.", 
+    "Noroway (nohr oh way)  1. Which.", 
+    "Nuv (nuhv) n.  1. Love.", 
+    "Oh (oh) adj.  1. Too.", 
+    "Ooba (oo bah) n.  1. Rain.", 
+    "Oodeed (oo deef) adj.  1. Sweet.", 
+    "Ota (oh tah)  1. To.", 
+    "Paamuk (pah mook) n.  1. Power.", 
+    "Pibooka (pih boo kah) n.  1. Clearing.", 
+    "Pola (poh lah) adj.  1. Polar.", 
+    "Powa (pow wah) n.  1. Power.", 
+    "Reh rehluu (reh reh loo) v.  1. Dance.", 
+    "Reshee (resh ee) n.  1. Map.", 
+    "Roda (roh dah) v.  1. To eat.", 
+    "Rueenee (roo ee nee) n.  1. Ruins.", 
+    "Ruha (roo hah) v.  1. Hit.", 
+    "Seefo (see foh) v.  1. Hurt.", 
+    "Sheeu (shee oo) n.  1. Name.", 
+    "Shetai (sheh tie) n.  1. Warrior.", 
+    "Shodu (sho doo) n.  1. Mother.", 
+    "Sirut (seer oot) n.  1. Door.", 
+    "Siz (sihz) n.  1. Fire.", 
+    "Sku (skoo)  1. Hello.", 
+    "Sleesh (sleesh) n.  1. Berry.", 
+    "Sta (stah).  1. Now.", 
+    "Stoja (sto jah) n.  1. Station.", 
+    "Sunee (suh nee) n.  1. Sun.", 
+    "Sut (sutt) adj.  1. Soon.", 
+    "Ta (tah) def. art.  1. The.", 
+    "Tal (tale) n.  1. Sun.", 
+    "Teeha (tee hah) v.  1. Thank you.", 
+    "Teeket (tee kett) n.  1. Heart.", 
+    "Thees (theess) adj.  1. Good.", 
+    "Theesa (thee sah) n.  1. Baby.  2. Child.", 
+    "Thek (theck) adj.  1. Here.", 
+    "T'hesh (tuh hesh) adj.  1. Quiet.", 
+    "Thuk (thuck) n.  1. Rock.", 
+    "Treek (treek) v.  1. Go.", 
+    "Treekthin (treek thin) n.  1. Journey.", 
+    "Treeta Dobra (tree tah Doh brah) n.  1. Council of Elders.", 
+    "Tu (too) def. art.  1. The.", 
+    "Tyatee (ty ah tee) v.  1. Come.", 
+    "Veek (veek) adj.  1. Quick.", 
+    "Weewa (wee wah) n.  1. House.  2. Home.", 
+    "X'eef (zheef) n.  1. Ground.", 
+    "X'iutha (zhoo thah) adj.  1. Important.", 
+    "Yayath (ya yath) v.  1. Jump.", 
+    "Yeh (yeh) adj.  1. Old.", 
+    "Yeha (yee hah)  1. Good-bye.", 
+    "Yehan (yeh han) n.  1. Peace.", 
+    "Yesh (yesh) adj.  1. Right.  2. Correct.", 
+    "Yiyult (yeye yult) n.  1. Cup.", 
+    "Yit (yitt) n.  1. Thing.", 
+    "Yub nub (yubb nubb) n.  1. Freedom.", 
+    "Yubnub (yub nub)  1. Hurray.", 
+    "Yud ehda (yood ay dah)  1. Alas.", 
+    "Yuf (yuhf) v.  1. Climb.", 
+    "Yungyet (yung yett) n.  1. Nut.", 
+    "Yupyup (yupp yupp)v.  1. Rejoice.", 
+    "Zeeg (zeeg) adj.  1. In.", 
+    "Zehg (zayg) adj.  1. Out."
+]
+
+function parse_dict_entry(entry) {
+    var defs = [];
+    for (var i = 1; true; i++) {
+        index = entry.indexOf(i + ". ");
+        if (index == -1) {
+            break;
+        }
+        end_index = entry.indexOf(i+1 + ". ");
+        if (end_index == -1) {
+            end_index = entry.length;
+        }
+        var def = entry.substring(index + 3, end_index).trim().toLowerCase();
+        matches = def.match(/[a-zA-Z]([ -'a-zA-Z])*/g);
+        defs.push(stemmer(matches[0]));
+    }
+    return defs;
+}
+
+basic_to_ewok = {};
+function build_map() {
+    for (var i = 0; i < text_dict.length; i++) {
+        defs = parse_dict_entry(text_dict[i]);
+        for (var j = 0; j < defs.length; j++) {        
+            basic_to_ewok[defs[j]] = text_dict[i];
+        }
+    }
+};
+
+function translate(basic) {
+    basic = basic.trim();
+    if (basic.length == 0) {
+        return;
+    }
+    words = basic.toLowerCase().match(/[a-zA-Z'-]+/g);
+    translated_sentence = "";
+    n = 3;
+    definitions = [];
+    for (i = 0; i < words.length; i++) {
+        for (n_size = Math.min(n, words.length-i); n_size >= 1; n_size--) {  // n-grams
+            n_gram = "";
+            for (ni = 0; ni < n_size; ni++) {
+                n_gram += " " + words[i + ni];
+            }
+            phrase = stemmer(n_gram.trim());
+            if (phrase in basic_to_ewok) {
+                definition = basic_to_ewok[phrase];
+                definitions.push(definition);
+                if (translated_sentence.length > 0) {
+                    definition = definition.toLowerCase();
+                }
+                translated_sentence += " " + definition.substring(0, definition.indexOf("(")).trim();
+                i += n_size - 1;
+                break;
+            }
+        }
+    }
+    $("#result-section").show();
+    if (definitions.length > 0) {
+        $("#result").text(translated_sentence.trim());
+        str = definitions[0];
+        for (i = 1; i < definitions.length; i++) {
+            str += "<br/>" + definitions[i];
+        }
+        $("#defs").html(str);
+    } else {
+        $("#result").text("No translation found :(");        
+        $("#defs").html("");
+    }
+}
+
+(function main() {
+    build_map();
+    $("#result-section").hide();    
+    $("#basic-input").keydown(function(event) {
+        if (event.keyCode === 13) {
+            translate($("#basic-input").val());
+        }
+    });
+    $("#translate-button").click(function() {
+        translate($("#basic-input").val());
+    });
+})();
